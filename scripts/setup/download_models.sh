c1c0234e2281eafac8dfb88241a3a656a5582e47
mainakmanna <moinak.manna@gmail.com>
2022-01-31 23:35:55 +0000

    Adds support for new reid model

--- scripts/setup/download_models.sh
+++ scripts/setup/download_models.sh
@@ -4,4 +4,4 @@ OUTPUT_PATH=$(python -c "from mot_neural_solver.path_cfg import OUTPUT_PATH; pri
 wget -P $OUTPUT_PATH/trained_models/reid https://vision.in.tum.de/webshare/u/brasoand/mot_neural_solver/resnet50_market_cuhk_duke.tar-232
 wget -P $OUTPUT_PATH/trained_models/frcnn https://vision.in.tum.de/webshare/u/brasoand/mot_neural_solver/frcnn_epoch_27.pt.tar
 wget -P $OUTPUT_PATH/trained_models/graph_nets https://vision.in.tum.de/webshare/u/brasoand/mot_neural_solver/mot_mpnet_epoch_006.ckpt
-
+cp /content/drive/MyDrive/output/models/model-best.pth.tar $OUTPUT_PATH/trained_models/reid/resnet50_ctmc.pth.tar
