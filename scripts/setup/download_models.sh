f61e1504d66eec2f298b3938afd4242dd2acee2c
mainakmanna <moinak.manna@gmail.com>
2022-02-02 23:46:24 +0000

    reid reused, config updated,reid model download

--- scripts/setup/download_models.sh
+++ scripts/setup/download_models.sh
@@ -5,3 +5,6 @@ wget -P $OUTPUT_PATH/trained_models/reid https://vision.in.tum.de/webshare/u/bra
 wget -P $OUTPUT_PATH/trained_models/frcnn https://vision.in.tum.de/webshare/u/brasoand/mot_neural_solver/frcnn_epoch_27.pt.tar
 wget -P $OUTPUT_PATH/trained_models/graph_nets https://vision.in.tum.de/webshare/u/brasoand/mot_neural_solver/mot_mpnet_epoch_006.ckpt
 
+cp /content/drive/MyDrive/output/models/model-best.pth.tar $OUTPUT_PATH/trained_models/reid/resnet50_ctmc.pth.tar
+cp /content/drive/MyDrive/output/models/model-best-split-1.pth.tar $OUTPUT_PATH/trained_models/reid/resnet50_ctmc_1.pth.tar
+cp /content/drive/MyDrive/output/models/reid-full.pth.tar $OUTPUT_PATH/trained_models/reid/resnet50_ctmc_full.pth.tar
